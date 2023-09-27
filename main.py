8e229c03f2be5f70cb21c4fd0ee61b292c3f6dcd
danylopriadko9 <danylopriadko9@gmail.com>
2023-09-27 23:25:21 +0200

    video detection

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import cv2
+import numpy as np
 
-img = cv2.imread('images/PRI_223554170.webp')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture(0)
+cap.set(3, 400)
+cap.set(4, 300)
 
-faces = cv2.CascadeClassifier('faces.xml')
+while True:
+    success, img = cap.read()
 
-results = faces.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = cv2.CascadeClassifier('faces.xml')
 
-for (x, y, w, h) in results:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+    result = faces.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
-cv2.imshow('Result', img)
-cv2.waitKey(0)
+    for (x, y, w, h) in result:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+
+    cv2.imshow('Result', img)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# img = cv2.imread('images/PRI_223554170.webp')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#
+# faces = cv2.CascadeClassifier('faces.xml')
+#
+# results = faces.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
+#
+# for (x, y, w, h) in results:
+#     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+#
+# cv2.imshow('Result', img)
+# cv2.waitKey(0)
